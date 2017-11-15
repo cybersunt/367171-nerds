701c8494eccc6ea7f31a455ca793fa70e1668b69
cybersunt <juliam2007@mail.ru>
2017-11-15 22:52:33 +0300

    Отладка кода

--- gulpfile.js
+++ gulpfile.js
@@ -3,52 +3,42 @@
 // Определим необходимые инструменты
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
-    rename = require("gulp-rename"),
+    rename = require('gulp-rename'),
 
     postcss = require('gulp-postcss'),
-    minify = require("gulp-csso"),
-    posthtml = require('gulp-posthtml'),
+    minify = require('gulp-csso'),
 
     importcss = require('postcss-import'),
     urlcss = require('postcss-url'),
     autoprefixer = require('autoprefixer'),
-    include = require('posthtml-include'),
 
     server = require('browser-sync').create();
 
 // ЗАДАЧА: Компиляция css
 gulp.task('style', function() {
-  return gulp.src('sourse/css/path/style.css')
+  return gulp.src('./source/css/path/style.css')
     .pipe(plumber())
     .pipe(postcss([
       importcss(),
       urlcss(),
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest('./build/css'))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest('./build/css'))
     .pipe(server.stream());
-});
-
-gulp.task('html', function() {
-  return gulp.src('sourse/*.html')
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest('build'));
 });
 
 gulp.task('serve', function() {
   return server.init({
-    server: '.',
+    server: './source',
     notify: false,
     open: true,
     cors: true,
     ui: false
 });
 
-  gulp.watch('./sourse/css/**/*.css', ['style']);
-  gulp.watch('./sourse/*.html').on('change', server.reload);
+  gulp.watch('./source/**/*.css', ['style']);
+  gulp.watch('./source/*.html').on('change', server.reload);
 });
